4addbda1ae3f0a85d641139dbc8a6d08592469f9
Scott Bender <scott@scottbender.net>
2020-01-16 10:48:13 -0500

    initial checking

---
+++ server/upnp-docker/build-images.sh
@@ -0,0 +1,31 @@
+ARCH=$1
+if [ "$ARCH" == "" ]; then
+  echo "specify arch: $ARCHS"
+  exit 1
+fi
+
+VER=$2
+
+if [ "$VER" == "" ]; then
+  echo "specify version"
+  exit 1
+fi
+
+REPO=victronenergy
+TARGET=venus-docker-upnp
+BUILD_OPTS=--no-cache
+TAG_LATEST=0
+ARCHS="armhf amd64"
+
+ARCH_TAGS=""
+for arch in $ARCHS
+do
+    ARCH_TAGS="$ARCH_TAGS $REPO/$TARGET:${arch}-${VER}"
+done
+
+docker build ${BUILD_OPTS} $PLATFORM -t $REPO/$TARGET:${ARCH}-${VER} .
+docker push $REPO/$TARGET:${ARCH}-${VER}
+
+docker manifest create $REPO/$TARGET:$VER $ARCH_TAGS
+docker manifest push $REPO/$TARGET:$VER
+rm -rf ~/.docker/manifests/docker.io_${REPO}_${TARGET}-${VER}
